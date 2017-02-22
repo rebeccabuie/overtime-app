dbf7377e8b613009af9d8a14cac00b39b8152277
rebeccabuie <rebeccabuie@gmail.com>
2017-02-21 21:05:53 -0700

    Integrated validations for audit log attributes

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,2 +1,6 @@
 class AdminUser < User
+  it 'it should have a start date equal to 6 days prior' do
+  new_audit_log = AuditLog.create(user_id: User.last.id)
+  expect(new_audit_log.start_date).to eq(Date.today - 6.days)
+end 
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  
+  before do
+    @audit_log = FactoryGirl.create(:audit_log)
+  end
+
+  describe 'creation' do
+    it 'can be properly created' do
+      expect(@audit_log).to be_valid
+    end
+  end
 end
